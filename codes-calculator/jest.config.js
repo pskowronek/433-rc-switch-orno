f3c65a20c3669d099ede7d13c31087c74c0d4126
Piotr Skowronek <piotr.skowronek@gmail.com>
2024-01-18 00:30:47 +0100

    Removed chatGPT generated tests that don't work

--- codes-calculator/jest.config.js
+++
@@ -1,7 +0,0 @@
-// jest.config.js
-module.exports = {
-  testEnvironment: 'jsdom',
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js$',
-  setupFiles: ['./jest.setup.js'],
-};
-
--- codes-calculator/jest.setup.js
+++
@@ -1,4 +0,0 @@
-// jest.setup.js
-global.TextEncoder = require('text-encoding').TextEncoder;
-global.TextDecoder = require('text-encoding').TextDecoder;
-
